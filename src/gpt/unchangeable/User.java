7815c9f46e5f6d41e875174ced301292b99e2405
swhyeon98 <smreo75@gmail.com>
2025-04-15 23:26:25 +0900

    gpt Java 실습 진단 과제 - 불변 객체 설계

---
+++ src/gpt/unchangeable/User.java
@@ -0,0 +1,20 @@
+package gpt.unchangeable;
+
+public class User {
+
+    private final String name;
+    private final Integer age;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
