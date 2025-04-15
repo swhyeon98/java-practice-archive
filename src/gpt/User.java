b20f1aa7109e76536cdb76250ffa189ab0a87279
swhyeon98 <smreo75@gmail.com>
2025-04-15 23:19:55 +0900

    gpt Java 실습 진단 과제 - Optional을 활용한 NPE 방지

---
+++ src/gpt/User.java
@@ -0,0 +1,15 @@
+package gpt;
+
+public class User {
+
+    private Long id;
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
