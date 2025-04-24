05081ae830f39196277592da8d21b5f0319df71c
swhyeon98 <smreo75@gmail.com>
2025-04-24 23:59:50 +0900

    다형성 2 - 다형성 활용2

--- src/poly/ex1/AnimalSoundMain.java
+++ src/poly/ex1/AnimalSoundMain.java
@@ -7,8 +7,6 @@ public class AnimalSoundMain {
         Cat cat = new Cat();
         Caw caw = new Caw();
 
-        Caw[] cawArr = {dog, cat, caw}; // 컴파일 오류 발생!
-
         System.out.println("동물 소리 테스트 시작");
         dog.sound();
         System.out.println("동물 소리 테스트 종료");
