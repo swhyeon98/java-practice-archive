b30841f2d129ab206f8f4733f7d05a09eb1e8c43
swhyeon98 <smreo75@gmail.com>
2025-08-01 19:52:52 +0900

    feat: 기본 구분자(, :)로 문자열 분리하여 합산 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,14 +2,26 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+        List<String> delimiter = new ArrayList<>(Arrays.asList(",", ":"));
 
         if (input.isEmpty()) {
             System.out.println("결과 : 0");
             return;
         }
+
+        List<String> numbers = Arrays.asList(input.split(String.valueOf(delimiter)));
+        int result = numbers.stream()
+                .mapToInt(Integer::parseInt)
+                .sum();
+
+        System.out.println("결과 : " + result);
     }
 }
