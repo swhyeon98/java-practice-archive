c33129fd1abf933b66d52937ef9d3d501115bf57
swhyeon98 <smreo75@gmail.com>
2025-08-11 22:59:24 +0900

    test: 음수 입력 예외 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -56,6 +56,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 음수_입력_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,-2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
