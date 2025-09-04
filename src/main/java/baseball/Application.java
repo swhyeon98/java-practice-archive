7d3686bc24f650a5235319d9a3a00458c0b80238
swhyeon98 <smreo75@gmail.com>
2025-09-04 23:45:57 +0900

    feat: 전체 게임 루프 연결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,8 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            runGameOnce();
+        } while (askRestartOrQuit() == 1);
     }
 
     private static List<Integer> getComputer() {
