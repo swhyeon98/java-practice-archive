4ca30151ed9fc9a8df1a1cee0b5a2ec830b015d8
swhyeon98 <smreo75@gmail.com>
2025-09-25 20:25:35 +0900

    feat: 리스트 전체 중복 검증 기능 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -21,6 +23,13 @@ public class Lotto {
             if (number > 45 || number < 0) {
                 throw new IllegalArgumentException("[ERROR] 로또 번호의 범위는 1~45입니다.");
             }
+        }
+    }
+
+    private void validateAllDistinct(List<Integer> numbers) {
+        Set<Integer> unique = new HashSet<>(numbers);
+        if (unique.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 다른 6개여야 합니다.");
         }
     }
 }
