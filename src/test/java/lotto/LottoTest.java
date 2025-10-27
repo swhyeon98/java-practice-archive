3b08a1d3661e2f9effa792dc1fa88a2d5b73c2f3
swhyeon98 <smreo75@gmail.com>
2025-10-27 23:59:52 +0900

    test: 숫자 개수·중복·범위 검증 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -17,10 +17,23 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void 숫자는_정확히_6개여야_한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageStartingWith("[ERROR]");
+    }
+
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자는_서로_달라야_한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 1, 2, 3, 4, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -35,11 +48,19 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void 숫자는_1에서_45_사이여야_한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(0, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Lotto(List.of(46, 2, 3, 4, 5, 6)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @ParameterizedTest
     @ValueSource(ints = {1, 45})
     void 로또_번호_경계값은_예외가_발생하지_않아야_한다(int boundary) {
         //given
-        List<Integer> numbers = List.of(boundary, 2,3,4,5,6);
+        List<Integer> numbers = List.of(boundary, 2, 3, 4, 5, 6);
 
         //when & then
         assertThatNoException().isThrownBy(() -> new Lotto(numbers));
