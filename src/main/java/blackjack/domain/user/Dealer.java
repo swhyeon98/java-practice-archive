9fd6f4995ea44e30e2c672c7d4ab9400156d8879
swhyeon98 <smreo75@gmail.com>
2025-12-09 20:01:57 +0900

    Initial commit

---
+++ src/main/java/blackjack/domain/user/Dealer.java
@@ -0,0 +1,21 @@
+package blackjack.domain.user;
+
+import blackjack.domain.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 게임 딜러를 의미하는 객체
+ */
+public class Dealer {
+    private final List<Card> cards = new ArrayList<>();
+
+    public Dealer() {}
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    // TODO 추가 기능 구현
+}
