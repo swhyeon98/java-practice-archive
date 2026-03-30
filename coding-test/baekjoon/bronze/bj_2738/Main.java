c6ba4c6b2c6f92e5da68df5ea8a7f705b8f7496c
swhyeon98 <smreo75@gmail.com>
2026-03-30 17:09:38 +0900

    feat: 백준 브론즈3 행렬 덧셈(2738) 풀이 추가

---
+++ coding-test/baekjoon/bronze/bj_2738/Main.java
@@ -0,0 +1,33 @@
+package bronze.bj_2738;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] array = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                array[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                array[i][j] += Integer.parseInt(st.nextToken());
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
