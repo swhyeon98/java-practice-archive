6c55613c53faccf4e665a847a0b428c69572ea2d
swhyeon98 <smreo75@gmail.com>
2026-04-02 10:11:44 +0900

    refactor: 프로그래머스 옹알이(120956) 각 발음은 한 번만 사용 가능 조건 부합하도록 수정

--- coding-test/programmers/level0/pg_120956/Solution.java
+++ coding-test/programmers/level0/pg_120956/Solution.java
@@ -3,14 +3,13 @@ package level0.pg_120956;
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
+        String regex = "^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$";
 
-        for(String s : babbling) {
-            s = s.replaceAll("aya|ye|woo|ma", "");
-            if(s.isEmpty()) {
+        for (String s : babbling) {
+            if (s.matches(regex)) {
                 answer++;
             }
         }
-
         return answer;
     }
 }
